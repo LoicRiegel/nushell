be76113a7e988c7ea91f302e72108a99166f6df0
Loïc Riegel <loic.riegel@outlook.fr>
2025-03-07 17:04:30 +0100

    other math commands can work with bounded ranges

--- crates/nu-command/src/math/variance.rs
+++ crates/nu-command/src/math/variance.rs
@@ -13,6 +13,7 @@ impl Command for SubCommand {
         Signature::build("math variance")
             .input_output_types(vec![
                 (Type::List(Box::new(Type::Number)), Type::Number),
+                (Type::Range, Type::Number),
                 (Type::table(), Type::record()),
                 (Type::record(), Type::record()),
             ])
